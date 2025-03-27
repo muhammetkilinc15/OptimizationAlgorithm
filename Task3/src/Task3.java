d6bda62682e637ecffdc8a9904f0b66def3f21a0
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2025-03-27 11:46:45 +0300

    Feat: Add Task3

---
+++ Task3/src/Task3.java
@@ -0,0 +1,113 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task3 {
+
+    // Toplam şehir sayısı (N = 14)
+    private static final int N = 14;
+
+    // Şehirlerin enlem ve boylam koordinatları
+    public static final double[][] cordinates = {
+            {16.47, 96.10}, {16.47, 94.44}, {20.09, 92.54}, {22.39, 93.37}, {25.23, 97.24},
+            {22.00, 96.05}, {20.47, 97.02}, {17.20, 96.29}, {16.30, 97.38}, {14.05, 98.12},
+            {16.53, 97.38}, {21.52, 95.59}, {19.41, 97.13}, {20.09, 94.55}
+    };
+
+    // Başlangıç için rastgele bir rota üretir (1'den 14'e kadar şehirler)
+    public static int[] generateInitialSolution() {
+        int[] nodes = new int[N];
+        for (int i = 0; i < N; i++) {
+            nodes[i] = i + 1; // Şehirler 1, 2, 3, ..., 14 şeklinde numaralandırılıyor
+        }
+        shuffleArray(nodes); // Rastgele karıştırılıyor
+        return nodes;
+    }
+
+    // Dizi elemanlarını karıştırmak için kullanılan yardımcı metot (Fisher-Yates Shuffle)
+    private static void shuffleArray(int[] array) {
+        Random rand = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    // Rastgele iki şehri yer değiştirerek yeni bir çözüm üretir
+    public static int[] swapTwoRandomElements(int[] solution) {
+        int[] arr = solution.clone(); // Orijinal çözümü bozmamak için kopya al
+        Random rand = new Random();
+        int i = rand.nextInt(N);
+        int j = rand.nextInt(N);
+        while (i == j) { // İkisi aynı olursa tekrar seç
+            j = rand.nextInt(N);
+        }
+        // Swap işlemi
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return arr;
+    }
+
+    // İki şehir arasındaki Öklid mesafesini hesaplar
+    public static double euclide(int index1, int index2) {
+        return Math.sqrt(Math.pow(cordinates[index1 - 1][0] - cordinates[index2 - 1][0], 2)
+                + Math.pow(cordinates[index1 - 1][1] - cordinates[index2 - 1][1], 2));
+    }
+
+    // Verilen bir çözüm dizisinin toplam rota maliyetini hesaplar
+    public static double calculateCost(int[] solution) {
+        double distAll = 0;
+        // Tüm şehirler arasındaki mesafeleri toplar
+        for (int i = 0; i < solution.length - 1; i++) {
+            distAll += euclide(solution[i], solution[i + 1]);
+        }
+        // Son şehirden tekrar başlangıç şehrine dönmeyi de ekler
+        distAll += euclide(solution[solution.length - 1], solution[0]);
+        return distAll;
+    }
+
+    // Simulated Annealing algoritmasını uygulayan metot
+    public static void simulatedAnnealing(double initialTemp, double coolingRate, int MaxIteration) {
+        // 1. Rastgele bir başlangıç çözümü üret
+        int[] currentSolution = generateInitialSolution();
+        double currentFitness = calculateCost(currentSolution);
+
+        double temperature = initialTemp; // Başlangıç sıcaklığı
+        Random rand = new Random();
+        int i = 0;
+
+        // 2. İterasyon başlasın
+        while (i < MaxIteration) {
+            // Komşu çözüm üret (2 şehir yer değiştir)
+            int[] newSolution = swapTwoRandomElements(currentSolution);
+            double newFitness = calculateCost(newSolution);
+
+            // 3. Kabul kriteri:
+            // Yeni çözüm daha iyi ise kabul et
+            // Daha kötü ise belirli bir olasılıkla kabul et (yerel minimumdan kaçmak için)
+            if (newFitness < currentFitness || rand.nextDouble() < Math.exp((currentFitness - newFitness) / temperature)) {
+                currentSolution = newSolution;
+                currentFitness = newFitness;
+            }
+
+            // 4. Sıcaklığı azalt (soğuma işlemi)
+            temperature *= coolingRate;
+
+            // Durum çıktısı
+            System.out.println("Iteration :" + i + " Best Fitness: " + currentFitness);
+            i++;
+        }
+
+        // 5. Sonuçları yazdır
+        System.out.println("Best route: " + Arrays.toString(currentSolution));
+        System.out.println("Best Fitness: " + currentFitness);
+    }
+
+    public static void main(String[] args) {
+        // Algoritmayı çalıştır
+        simulatedAnnealing(1000, 0.995, 100000);
+        // 1000 başlangıç sıcaklığı, 0.995 soğuma oranı, 100.000 iterasyon
+    }
+}
