0be001bef2ee8a9db5c4ba503fac86d7da70dc34
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2025-03-27 12:47:06 +0300

    Feat: Add hill climbing optimization algorithm

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
 public class Main {
-
     // Bu Task'ta Random bir n array oluşturulacak ve bu array üzerinde iterasyonlar yapılacak.
     // Her iterasyonda bir rastgele index seçilip bu index'teki değer rastgele bir değerle değiştirilecek.
     // Eğer yeni değer eski değerden daha iyi bir sonuç veriyorsa yeni değer kabul edilecek.
@@ -9,51 +8,75 @@ public class Main {
     // [-100, 100] aralığında rastgele değerler alınacak.
     // 1000 iterasyon yapılacak ve sonuç yazdırılacak.
     // Sonuçta en iyi sonucu veren array yazdırılacak.
+
+    public static int N = 10; // Dizinin boyutu
     public static void main(String[] args) {
+        Random random = new Random();  // Rastgele değerler oluşturmak için Random sınıfı
+        int[] currentSolution = generateInitialSolution(random); // Başlangıç çözümünü oluştur
+        int maxIt = 1000;  // Maksimum iterasyon sayısı
 
-        int[] currentSol = new int[10]; // Random n array
-        Random random = new Random();
+        // Hill Climbing algoritması - her iterasyonda çözüm iyileştirilir
+        for (int i = 0; i < maxIt; i++) {
+            // Yeni çözüm üret
+            int[] newSolution = movement(currentSolution, random);
 
-        // [-100, 100] doldur
-        for (int i = 0; i < currentSol.length; i++) {
-            currentSol[i] = random.nextInt(-100, 100);
-        }
-
-        int maxIt = 1000;
-        int it = 0;
-        while (it < maxIt) {
-            int[] newArr = movement(currentSol);
-            if (fitness(newArr) < fitness(currentSol)) {
-                currentSol = newArr.clone();
+            // Yeni çözüm eski çözümden daha iyi ise, onu kabul et
+            if (fitness(currentSolution) > fitness(newSolution)) {
+                currentSolution = newSolution.clone();  // Yeni çözümü kopyala
             }
-            System.out.println("Iteration: " + (it + 1) + "  Value: " + fitness(currentSol));
-            it++;
+
+            // Her iterasyonun fitness değerini yazdır
+            System.out.println("Iteration " + (i+1) + ": " + fitness(currentSolution));
         }
+
+        // Sonuçları yazdır
         System.out.println("Final Solution:");
-        for (int value : currentSol) {
+        printSolution(currentSolution);
+    }
+    
+    // Çözümü ekrana yazdırmak için yardımcı metod
+    public static void printSolution(int[] solution) {
+        for (int value : solution) {
             System.out.print(value + " ");
         }
+        System.out.println();  // Yeni satır
     }
 
-
-    public static int[] movement(int[] currentSol) {
-        Random rand = new Random();
-        int[] newArr = currentSol.clone();
-        int index = rand.nextInt(10);
-        int randomNumber = rand.nextInt(-5, 5);
-        newArr[index] += randomNumber;
-        if (newArr[index] < -100)
-            newArr[index] = -100;
-        if (newArr[index] > 100)
-            newArr[index] = 100;
-        return newArr;
-    }
-
-    public static int fitness(int[] currentSol) {
-        int sum = 0;
-        for (int value : currentSol) {
-            sum += (int) Math.pow(value, 2);
+    // Başlangıç çözümünü rastgele oluşturur
+    public static int[] generateInitialSolution(Random random) {
+        int[] initialSolution = new int[N];
+        // Her elemanı [-100, 100] aralığında rastgele bir değerle başlat
+        for (int i = 0; i < N; i++) {
+            initialSolution[i] = random.nextInt(-100, 101);  // -100 ile 100 arası
         }
-        return sum;
+        return initialSolution;
+    }
+
+    // Hareket fonksiyonu: Rastgele bir indeksi seçer, bu indeksteki değeri değiştirir
+    public static int[] movement(int[] currentSolution, Random random) {
+        int[] newSolution = currentSolution.clone();  // Mevcut çözümü kopyala
+        int index1 = random.nextInt(N);  // Rastgele bir indeks seç
+        int value = random.nextInt(-5, 5);  // -5 ile 5 arasında rastgele bir değer seç
+        newSolution[index1] += value;  // Seçilen indeksteki değeri güncelle
+
+        // Yeni değeri sınırlamak: -100 ile 100 arasında tut
+        if (newSolution[index1] > 100) {
+            newSolution[index1] = 100;
+        }
+        if (newSolution[index1] < -100) {
+            newSolution[index1] = -100;
+        }
+
+        return newSolution;  // Yeni çözümü döndür
+    }
+
+    // Fitness fonksiyonu: Dizideki tüm elemanların karelerinin toplamı
+    public static double fitness(int[] solution) {
+        double sum = 0;
+        // Her elemanın karesini alıp toplar
+        for (int i = 0; i < N; i++) {
+            sum += Math.pow(solution[i], 2);  // x[i]^2 yerine x[i] * x[i] kullanabiliriz
+        }
+        return sum;  // Fitness değeri
     }
 }
