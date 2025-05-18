dee10c1f582b3d134ced09ccae18902662481cc1
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2025-05-18 14:33:39 +0300

    feat: Add task5

--- Task6/src/Task/Task.java
+++ Task6/src/Task/Task.java
@@ -1,79 +1,101 @@
 package Task;
 
 import java.util.Random;
+/*
+     Problem : Dikdörtgen şeklinde bir tarlamız var alanı 400 m^2
+     Biz bu tarlaya 3 farklı türde şeftali ağaçları dikmek istiyoruz.
+     Her türdeki ağaçların kapladıkları alan ve kazandırdıkları para farklıdır.
+     Ağaçlarımız 3 farklı ağacımız a,b,c olsun.
+     Ağaçların kapladıklaır alanlar sırası ile 3,5,7 m^2
+     Ağaçların kazandıkları para sırası ile 10,20,30 TL
+
+     Kısıtlamalar:
+        * Ekdiğimiz ağaçlar toplam 400 m^2 den küçük ya da eşit olmalıdır.
+        * Bir ağaç türünün maximum sayısı toplam ağaç sayısına oranla 0.5den buyuk olamaz.
+
+
+    Fitness Fonksiyonu:
+        * Ağaçların kazandıkları para toplamını return eder.
+
+    Constraint Fonksiyonları:
+        * Bir ağaç türünün maximum sayısı toplam ağaç sayısına oranla 0.5den buyuk olamaz.
+        * Ekdiğimiz ağaçlar toplam 400 m^2 den küçük ya da eşit olmalıdır.
+
+     Görüntülenen sonuçlar :
+        * En iyi sonuç
+        * En iyi sonuçta bulunan ağaç sayısı
+ */
+
 
 public class Task {
 
     public static void main(String[] args) {
-        int maxIt = 20;
+        int maxIt = 100;
         int dim = 3;
         int np = 10;
         float c1 = 2, c2 = 2;
-        double[][] p = new double[np][];
-        double[][] pBest = new double[np][];
-        double[] gbest = new double[dim];
-        double[] profits = {10, 20, 30}; // profiletler
-        double[] areas = {3, 5, 7}; // alanlar
-        double gFit = Double.MAX_VALUE;
-        double[][] v = new double[np][];
+        int[][] p = new int[np][dim];
+        int[][] pBest = new int[np][dim];
+        int[] gBest = new int[dim];
+        double gFit = 0;
+        double[][] v = new double[np][dim];
+
+        int[] profits = {10, 20, 30}; // profiletler
+        int[] areas = {3, 5, 7}; // alanlar
+
         Random random = new Random();
         for (int i = 0; i < np; i++) {
-            v[i] = new double[dim];
-            p[i] = new double[dim];
-            pBest[i] = new double[dim];
-            for (int j = 0; j < dim; j++) {
-                p[i][j] = random.nextInt(0, 20);
-                pBest[i][j] = p[i][j];
-                v[i][j] = 0;
-            }
+            do {
+                for (int j = 0; j < dim; j++) {
+                    p[i][j] = random.nextInt(20);
+                    pBest[i][j] = p[i][j];
+                    v[i][j] = 0;
+                }
+            } while (!(constraint2(p[i], areas) && constraint1(p[i])));
         }
 
         for (int t = 0; t < maxIt; t++) {
             for (int i = 0; i < np; i++) {
-                double pb = fitness(pBest[i], profits);
-                double fb = fitness(p[i], profits);
-                // updatePbest
-                if (pb < fb && isValid(p[i]) && isValid(p[i], areas)) {
-                    for (int j = 0; j < dim; j++) {
-                        pBest[i][j] = p[i][j];
-                    }
+                double currentFitness  = fitness(p[i], profits);
+                double personalBestFitness  = fitness(pBest[i], profits);
+
+                // update PBest
+                if (personalBestFitness  < currentFitness  && constraint1(p[i]) && constraint2(p[i], areas)) {
+                    System.arraycopy(p[i], 0, pBest[i], 0, dim);
                 }
                 // update GBest
-                if (gFit > fb && isValid(p[i]) && isValid(p[i], areas)) {
-                    for (int j = 0; j < dim; j++) {
-                        gbest[j] = p[i][j];
-                    }
-                    gFit = fb;
+                if (gFit < currentFitness  && constraint1(p[i]) && constraint2(p[i], areas)) {
+                    System.arraycopy(p[i], 0, gBest, 0, dim);
+                    gFit = currentFitness ;
                 }
             }
             System.out.println("Iteration " + (t + 1) + ": " + gFit);
             for (int i = 0; i < np; i++) {
                 for (int j = 0; j < dim; j++) {
                     v[i][j] = v[i][j] + (c1 * Math.random() * (pBest[i][j] -
-
-                            p[i][j])) + (c2 * Math.random() * (gbest[j] - p[i][j]));
-                    p[i][j] = p[i][j] + v[i][j];
+                            p[i][j])) + (c2 * Math.random() * (gBest[j] - p[i][j]));
+                    p[i][j] = (int) (p[i][j] + v[i][j]);
                 }
             }
         }
 
         System.out.print("Best Solution: ");
         for (int j = 0; j < dim; j++) {
-            System.out.print(gbest[j] + ", ");
+            System.out.print(gBest[j] + ", ");
         }
 
     }
 
-    public static boolean isValid(double[] x) {
-        for (int i = 0; i < x.length; i++) {
-            if (x[i] / sum(x) > 0.5) {
+    public static boolean constraint1(int[] x) {
+        for (double v : x) {
+            if (v / sum(x) > 0.5) {
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean isValid(double[] x, double[] areas) {
+    public static boolean constraint2(int[] x, int[] areas) {
         double sum = 0;
         for (int i = 0; i < x.length; i++) {
             sum += x[i] * areas[i];
@@ -81,7 +103,7 @@ public class Task {
         return sum <= 400;
     }
 
-    public static double sum(double[] x) {
+    public static double sum(int[] x) {
         double sum = 0;
         for (double v : x) {
             sum += v;
@@ -89,7 +111,7 @@ public class Task {
         return sum;
     }
 
-    public static double fitness(double[] x, double[] profits) {
+    public static double fitness(int[] x, int[] profits) {
         double sum = 0;
         for (int i = 0; i < x.length; i++) {
             sum += x[i] * profits[i];
